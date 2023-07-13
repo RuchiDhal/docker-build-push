4b3b19a02fd63ab08e9c34273ef6b6eef1a0af85
Subhashree Dhal <subhashreedhal01@gmail.com>
2023-07-13 22:25:08 +0000

    changes done

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 docker build -t nginx:v1 .
-#check if the docker hub credentials are present or not!!
-if ! docker login -u <username> -p <password> &> /dev/null; then
-echo "Docker hub credentials not found trying to find credentials from environment variable"
-else
-echo "using  Docker hub credentials from environment varibale"
 #check if Environment variables are present or not
 if [[ -z "${DOCKER_USERNAME}" ]]; then
-  echo "enviroment variable DOCKER_USERNAME not found"
+  echo "ERROR - enviroment variable DOCKER_USERNAME not found"
   exit 1
 else
    echo "enviromnet variable DOCKER_USERNAME found. Using this"
@@ -23,4 +18,8 @@ docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker tag nginx:v1 ${DOCKER_USERNAME}/nginx:v1
 #push the docker image to dockerhub
 docker push ${DOCKER_USERNAME}/nginx:v1
-echo "docker image pushed successfully"
+if [ $(echo $?) == 0 ]; then
+ echo "docker image pushed successfully"
+else 
+ echo "ERROR"
+fi 
